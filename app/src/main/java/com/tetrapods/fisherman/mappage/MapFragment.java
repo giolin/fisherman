cb3746e911263ae161e7b4c70687196e3c497ce1
monotonique <georgelin422@gmail.com>
2018-02-03 17:19:42 +0800

    Add more layers

--- app/src/main/java/com/tetrapods/fisherman/mappage/MapFragment.java
+++ app/src/main/java/com/tetrapods/fisherman/mappage/MapFragment.java
@@ -1,15 +1,20 @@
 package com.tetrapods.fisherman.mappage;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.graphics.Color;
+import android.graphics.PointF;
+import android.graphics.RectF;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mapbox.mapboxsdk.Mapbox;
@@ -18,13 +23,13 @@ import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.plugins.geojson.GeoJsonPlugin;
-import com.mapbox.mapboxsdk.plugins.geojson.GeoJsonPluginBuilder;
 import com.mapbox.mapboxsdk.plugins.geojson.listener.OnLoadingGeoJsonListener;
 import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerMode;
 import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
 import com.mapbox.mapboxsdk.style.layers.FillLayer;
 import com.mapbox.mapboxsdk.style.layers.Filter;
+import com.mapbox.mapboxsdk.style.layers.LineLayer;
+import com.mapbox.mapboxsdk.style.layers.Property;
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 import com.mapbox.services.android.telemetry.location.LocationEngine;
@@ -33,7 +38,9 @@ import com.mapbox.services.android.telemetry.location.LocationEnginePriority;
 import com.mapbox.services.android.telemetry.location.LostLocationEngine;
 import com.mapbox.services.android.telemetry.permissions.PermissionsListener;
 import com.mapbox.services.android.telemetry.permissions.PermissionsManager;
+import com.mapbox.services.commons.geojson.Feature;
 import com.tetrapods.fisherman.R;
+import com.tetrapods.fisherman.util.SanctuaryDialog;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -47,10 +54,18 @@ import dagger.android.support.DaggerFragment;
 import timber.log.Timber;
 
 public class MapFragment extends DaggerFragment implements MapContract.View, OnMapReadyCallback,
-        LocationEngineListener, OnLoadingGeoJsonListener {
+        LocationEngineListener, OnLoadingGeoJsonListener, MapboxMap.OnMapClickListener {
 
     private static final String ACCESS_TOKEN = "pk.eyJ1IjoiZ2VvcmdlbGluNDIyIiwiYSI6ImNqYzdqenNwbjJjMnEyd24yZHM5MnhucHMifQ.FqZz7XTRMrHl1A9VzwtxcQ";
-    private static final String GEOJSON_SOURCE_ID = "GEOJSONFILE";
+    private static final String GEO_JSON_MY_ROUTE_SOURCE_ID = "GEO_JSON_MY_ROUTE_SOURCE_ID";
+    private static final String GEO_JSON_MY_ROUTE_LAYER_ID = "GEO_JSON_MY_ROUTE_LAYER_ID";
+    private static final String GEO_JSON_ECONOMY_SEA_SOURCE_ID = "GEO_JSON_ECONOMY_SEA_SOURCE_ID";
+    private static final String GEO_JSON_ECONOMY_SEA_LAYER_ID = "GEO_JSON_ECONOMY_SEA_LAYER_ID";
+    private static final String GEO_JSON_MARINE_SANCTUARY_SOURCE_ID = "GEO_JSON_MARINE_SANCTUARY_SOURCE_ID";
+    private static final String GEO_JSON_MARINE_SANCTUARY_LAYER_ID = "GEO_JSON_MARINE_SANCTUARY_LAYER_ID";
+    private static final String GEO_JSON_PORT = "GEO_JSON_PORT";
+    private static final String GEO_JSON_FISH_DISTRIBUTION = "GEO_JSON_FISH_DISTRIBUTION";
+    private static final int ZOOM = 11;
 
     @BindView(R.id.mapView)
     MapView mapView;
@@ -63,7 +78,9 @@ public class MapFragment extends DaggerFragment implements MapContract.View, OnM
     private LocationEngine locationEngine;
     private PermissionsManager permissionsManager;
     private Activity parentActivity;
-    private GeoJsonPlugin geoJsonPlugin;
+    private LineLayer myRouteLayer;
+    private FillLayer economySeaLayer;
+    private FillLayer marineSanctuaryLayer;
 
     @Inject
     public MapFragment() {
@@ -91,15 +108,10 @@ public class MapFragment extends DaggerFragment implements MapContract.View, OnM
     @Override
     public void onMapReady(MapboxMap mapboxMap) {
         this.mapboxMap = mapboxMap;
-//        createGeoJsonSource();
-//        addPolygonLayer();
-        Timber.d("onMapReady");
-        geoJsonPlugin = new GeoJsonPluginBuilder()
-                .withContext(parentActivity)
-                .withMap(this.mapboxMap)
-                .withOnLoadingFileAssets(this)
-                .build();
-        geoJsonPlugin.setAssetsName("data.geojson");
+        mapboxMap.addOnMapClickListener(this);
+        createGeoJsonSource();
+        addPolygonLayer();
+        addLineLayer();
         enableLocationPlugin();
         getLifecycle().addObserver(locationLayerPlugin);
     }
@@ -161,7 +173,7 @@ public class MapFragment extends DaggerFragment implements MapContract.View, OnM
 
     private void setCameraPosition(Location location) {
         mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                new LatLng(location.getLatitude(), location.getLongitude()), 16));
+                new LatLng(location.getLatitude(), location.getLongitude()), ZOOM));
     }
 
     @Override
@@ -223,19 +235,54 @@ public class MapFragment extends DaggerFragment implements MapContract.View, OnM
 
     private void createGeoJsonSource() {
         // Load data from GeoJSON file in the assets folder
-        GeoJsonSource geoJsonSource = new GeoJsonSource(GEOJSON_SOURCE_ID,
+        GeoJsonSource sourceMarineSanctuary = new GeoJsonSource(GEO_JSON_MARINE_SANCTUARY_SOURCE_ID,
+                loadJsonFileFromAssets("taiwan_north_east_marine_sanctuary.geojson"));
+        mapboxMap.addSource(sourceMarineSanctuary);
+        Timber.d("feature %s", sourceMarineSanctuary.querySourceFeatures(null));
+        GeoJsonSource geoJsonSource = new GeoJsonSource(GEO_JSON_ECONOMY_SEA_SOURCE_ID,
                 loadJsonFileFromAssets("data.geojson"));
         mapboxMap.addSource(geoJsonSource);
+        GeoJsonSource geoJsonMyRouteSoure = new GeoJsonSource(GEO_JSON_MY_ROUTE_SOURCE_ID,
+                loadJsonFileFromAssets("ship_route1.geojson"));
+//        List<Feature> features = geoJsonSource.getAttribution();
+//        for (Feature feature : features) {
+//            Timber.d("%s, %s, %s",
+//                    feature.getId(),
+//                    feature.getProperties(),
+//                    feature.getProperty("name"));
+//        }
+        mapboxMap.addSource(geoJsonMyRouteSoure);
     }
 
     private void addPolygonLayer() {
-        // Create and style a FillLayer that uses the Polygon Feature's coordinates in the GeoJSON data
-        FillLayer borderOutlineLayer = new FillLayer("polygon", GEOJSON_SOURCE_ID);
-        borderOutlineLayer.setProperties(
+        economySeaLayer = new FillLayer(GEO_JSON_ECONOMY_SEA_LAYER_ID,
+                GEO_JSON_ECONOMY_SEA_SOURCE_ID);
+        economySeaLayer.setProperties(
+                PropertyFactory.visibility(Property.VISIBLE),
                 PropertyFactory.fillColor(Color.RED),
                 PropertyFactory.fillOpacity(.4f));
-        borderOutlineLayer.setFilter(Filter.eq("$type", "Polygon"));
-        mapboxMap.addLayer(borderOutlineLayer);
+        economySeaLayer.setFilter(Filter.eq("$type", "Polygon"));
+        mapboxMap.addLayer(economySeaLayer);
+        marineSanctuaryLayer = new FillLayer(GEO_JSON_MARINE_SANCTUARY_LAYER_ID,
+                GEO_JSON_MARINE_SANCTUARY_SOURCE_ID);
+        marineSanctuaryLayer.setProperties(
+                PropertyFactory.visibility(Property.VISIBLE),
+                PropertyFactory.fillColor(Color.RED),
+                PropertyFactory.fillOpacity(.4f));
+        marineSanctuaryLayer.setFilter(Filter.eq("$type", "Polygon"));
+        mapboxMap.addLayer(marineSanctuaryLayer);
+    }
+
+    private void addLineLayer() {
+        myRouteLayer = new LineLayer(GEO_JSON_MY_ROUTE_LAYER_ID,
+                GEO_JSON_MY_ROUTE_SOURCE_ID);
+        myRouteLayer.setProperties(
+                PropertyFactory.lineCap(Property.LINE_CAP_ROUND),
+                PropertyFactory.lineJoin(Property.LINE_JOIN_ROUND),
+                PropertyFactory.lineWidth(5f),
+                PropertyFactory.lineColor(Color.BLACK)
+        );
+        mapboxMap.addLayer(myRouteLayer);
     }
 
     private String loadJsonFileFromAssets(String filename) {
@@ -266,5 +313,59 @@ public class MapFragment extends DaggerFragment implements MapContract.View, OnM
     public void onLoadFailed(Exception e) {
         Timber.e(e, "onLoadFailed");
 
+    }
+
+    @Override
+    public void showMapLayer(int type, boolean show) {
+        switch (type) {
+            case MapContract.MY_ROUTE: {
+                if (show) {
+                    myRouteLayer.setProperties(PropertyFactory.visibility(Property.VISIBLE));
+                } else {
+                    myRouteLayer.setProperties(PropertyFactory.visibility(Property.NONE));
+                }
+                break;
+            }
+            case MapContract.ECONOMY_SEA: {
+                if (show) {
+                    economySeaLayer.setProperties(PropertyFactory.visibility(Property.VISIBLE));
+                } else {
+                    economySeaLayer.setProperties(PropertyFactory.visibility(Property.NONE));
+                }
+                break;
+            }
+            case MapContract.MARINE_SANCTUARY: {
+                if (show) {
+                    marineSanctuaryLayer.setProperties(PropertyFactory.visibility(Property.VISIBLE));
+                } else {
+                    marineSanctuaryLayer.setProperties(PropertyFactory.visibility(Property.NONE));
+                }
+                break;
+            }
+            case MapContract.PORT: {
+                break;
+            }
+            case MapContract.FISH_DISTRIBUTION: {
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng point) {
+        Timber.d("onMapClick");
+        PointF pointf = mapboxMap.getProjection().toScreenLocation(point);
+        RectF rectF = new RectF(pointf.x - 10, pointf.y - 10, pointf.x + 10,
+                pointf.y + 10);
+        List<Feature> featureList = mapboxMap.queryRenderedFeatures(rectF,
+                GEO_JSON_MARINE_SANCTUARY_LAYER_ID);
+        for (com.mapbox.services.commons.geojson.Feature feature : featureList) {
+            final Dialog dialog = new SanctuaryDialog(parentActivity,
+                    feature.getStringProperty("name"));
+            dialog.show();
+        }
     }
 }
