1bae54736dc5a7b6a641911219854bf6c0a327b8
Ann <linlinweiwei0209@gmail.com>
2018-02-03 16:05:41 +0800

    law icon

---
+++ app/src/main/java/com/tetrapods/fisherman/law/LawListAdapter.java
@@ -0,0 +1,11 @@
+package com.tetrapods.fisherman.law;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Ann on 03/02/2018.
+ */
+
+public class LawListAdapter extends RecyclerView.Adapter {
+
+}
