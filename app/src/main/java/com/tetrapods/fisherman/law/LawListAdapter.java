294d5d5358c6d7e58b22c6935594bfaf83ce9201
Ann <linlinweiwei0209@gmail.com>
2018-02-04 12:43:25 +0800

    add laws dialog

--- app/src/main/java/com/tetrapods/fisherman/law/LawListAdapter.java
+++ app/src/main/java/com/tetrapods/fisherman/law/LawListAdapter.java
@@ -1,11 +1,15 @@
 package com.tetrapods.fisherman.law;
 
+import android.app.Dialog;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.TextView;
 import com.tetrapods.fisherman.R;
 import com.tetrapods.fisherman.law.LawListAdapter.ViewHolder;
 
@@ -14,8 +18,8 @@ import com.tetrapods.fisherman.law.LawListAdapter.ViewHolder;
  */
 
 public class LawListAdapter extends RecyclerView.Adapter<ViewHolder> {
-  String[] mLaws = {"流刺網法","底拖網法"};
-  int[] resIcon = {R.drawable.flow_gill_net,R.drawable.bottom_trawl};
+  String[] mLaws = {"流刺網法","底拖網法","中層拖網法"};
+  int[] resIcon = {R.drawable.flow_gill_net,R.drawable.bottom_trawl,R.drawable.middle_fishing};
 
   @Override
   public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -26,6 +30,7 @@ public class LawListAdapter extends RecyclerView.Adapter<ViewHolder> {
 
   @Override
   public void onBindViewHolder(ViewHolder holder, int position) {
+    holder.itemView.setTag(position);
     holder.mCheckBox.setText(mLaws[position]);
     holder.mIcon.setImageResource(resIcon[position]);
   }
@@ -35,7 +40,7 @@ public class LawListAdapter extends RecyclerView.Adapter<ViewHolder> {
     return mLaws.length;
   }
 
-  public class ViewHolder extends RecyclerView.ViewHolder{
+  public class ViewHolder extends RecyclerView.ViewHolder implements OnClickListener {
     public ImageView mIcon;
     public CheckBox mCheckBox;
 
@@ -43,8 +48,16 @@ public class LawListAdapter extends RecyclerView.Adapter<ViewHolder> {
       super(itemView);
       mCheckBox = itemView.findViewById(R.id.checkbox_law);
       mIcon = itemView.findViewById(R.id.law_icon);
+      itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+      Dialog dialog = new Dialog(view.getContext());
+      dialog.setContentView(R.layout.dialog_law);
+      TextView title = dialog.findViewById(R.id.title);
+      title.setText(mLaws[(int)view.getTag()]);
+      dialog.show();
     }
   }
-
-
 }
